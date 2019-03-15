e7a3e06d8fda3010cd87cff03471d2224cf575a2
BT_1N3_22@BKTECH.LOCAL <BT_1N3_22@BKTECH.LOCAL>
2019-03-15 10:11:06 -0400

    not done

--- First Platformer/Assets/coinFound.cs
+++ First Platformer/Assets/coinFound.cs
@@ -2,13 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class coinFound : MonoBehaviour
-{
+public class coinFound : MonoBehaviour { 
+public GameObject currentInterObj = null;
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("coin")) {
             Debug.Log(other.name);
+            currentInterObj = other.gameObject;
+
         }
       
+    }
+    void onTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("coin"))
+        {
+            if(other.gameObject == currentInterObj)
+            {
+                currentInterObj = null;
+            }
+        }
     }
 }
