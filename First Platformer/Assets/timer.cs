a1792f37c1dea8a84fac71bd4725207b4b78d52e
Jonathan <jlin7500@bths.edu>
2019-03-17 10:30:28 -0400

    ad
    asda

--- First Platformer/Assets/timer.cs
+++ First Platformer/Assets/timer.cs
@@ -1,13 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
-public class Score : MonoBehaviour
+public class timer : MonoBehaviour
 {
-
-    public Transform player;
-    public Text scoreText; 
     // Start is called before the first frame update
     void Start()
     {
@@ -17,7 +13,6 @@ public class Score : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        scoreText.text = "Score" + " " + player.position.x.ToString("0");
         
     }
 }
