5d0d7579724349d2e039c13f3c44a68d953b3dba
Jonathan <jlin7500@bths.edu>
2019-03-17 10:38:17 -0400

    asd
    asd

--- First Platformer/Assets/timer.cs
+++ First Platformer/Assets/timer.cs
@@ -1,9 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class timer : MonoBehaviour
 {
+    public Text Timer;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,6 @@ public class timer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Timer.text = 
     }
 }
