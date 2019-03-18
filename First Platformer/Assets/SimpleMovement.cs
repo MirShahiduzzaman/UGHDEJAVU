27e8985db8ae6bfb320263cf57bbd1f8c36b56bb
Mir Shahiduzzaman <mshahiduzzaman8068@bths.edu>
2019-03-17 23:17:38 -0400

    started to program movement of non-AI objects

---
+++ First Platformer/Assets/SimpleMovement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleMovement : MonoBehaviour
+{
+    public float speed = 300f;
+    public float distance;
+
+    private bool movingRight = true;
+    public bool vertical = false;
+
+    public Transform groundDetection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!vertical)
+        {
+            transform.Translate(Vector2.right * speed * Time.deltaTime);
+        }
+        else
+        {
+            transform.Translate(Vector2.down * speed * Time.deltaTime);
+        }
+
+        RaycastHit2D groundInfo = Physics2D.Raycast(groundDetection.position, Vector2.down, distance);
+
+        if(groundInfo.collider == false)
+        {
+            if(movingRight == true)
+            {
+                transform.eulerAngles = new Vector3(0,-180,0);
+                movingRight = false;
+            }
+            else
+            {
+                transform.eulerAngles = new Vector3(0,0,0);
+                movingRight = true;
+            }
+        }
+    }
+}
