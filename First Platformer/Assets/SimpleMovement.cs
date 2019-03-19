15552e9b115be2e904a1d6ad097c45a0c7d5a1a7
BT_1N3_23@BKTECH.LOCAL <BT_1N3_23@BKTECH.LOCAL>
2019-03-19 09:19:00 -0400

    finished simple movement script

--- First Platformer/Assets/SimpleMovement.cs
+++ First Platformer/Assets/SimpleMovement.cs
@@ -4,14 +4,12 @@ using UnityEngine;
 
 public class SimpleMovement : MonoBehaviour
 {
-    public float speed = 300f;
+    public float speed = 10f;
     public float distance;
     private float relDistance = 0f;
 
     private bool movingRight = true;
     public bool vertical = false;
-
-    public Transform groundDetection;
 
     // Start is called before the first frame update
     void Start()
@@ -25,27 +23,44 @@ public class SimpleMovement : MonoBehaviour
         if (!vertical)
         {
             transform.Translate(Vector2.right * speed * Time.deltaTime);
+
+            relDistance++;
+
+            if (relDistance >= distance)
+            {
+                if (movingRight == true)
+                {
+                    transform.eulerAngles = new Vector3(0, -180, 0);
+                    movingRight = false;
+                }
+                else
+                {
+                    transform.eulerAngles = new Vector3(0, 0, 0);
+                    movingRight = true;
+                }
+                relDistance = 0f;
+            }
         }
         else
         {
             transform.Translate(Vector2.down * speed * Time.deltaTime);
-        }
 
-        relDistance++;
-            
-        if((relDistance >= distance && movingRight) || (relDistance >= (2 * distance) && !movingRight))
-        {
-            if(movingRight == true)
+            relDistance++;
+
+            if (relDistance >= distance)
             {
-                transform.eulerAngles = new Vector3(0,-180,0);
-                movingRight = false;
+                if (movingRight == true)
+                {
+                    transform.eulerAngles = new Vector3(-180, 0, 0);
+                    movingRight = false;
+                }
+                else
+                {
+                    transform.eulerAngles = new Vector3(0, 0, 0);
+                    movingRight = true;
+                }
+                relDistance = 0f;
             }
-            else
-            {
-                transform.eulerAngles = new Vector3(0,0,0);
-                movingRight = true;
-            }
-            relDistance = 0f;
         }
     }
 }
