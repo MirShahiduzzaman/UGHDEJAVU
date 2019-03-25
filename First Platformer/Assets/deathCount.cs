f7e15d7dca413a29d6792302abce6941917a4110
Jonathan Lin <jlin7500@bths.edu>
2019-03-25 10:10:34 -0400

    das
    ad

---
+++ First Platformer/Assets/deathCount.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class deathCount : MonoBehaviour
+{
+    public Text DeathCount;
+    // Update is called once per frame
+    void Update()
+    {
+        DeathCount.text = "DeathCount" + " " + " " + " " + " ";
+
+    }
+}
