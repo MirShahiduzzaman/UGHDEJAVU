49f5c32dc8c7dea2a0912f1ac50c1c1b72e4b53d
BT_1N3_22@BKTECH.LOCAL <BT_1N3_22@BKTECH.LOCAL>
2019-06-10 10:14:26 -0400

    not odne

--- First Platformer/Assets/starTouch.cs
+++ First Platformer/Assets/starTouch.cs
@@ -1,18 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class starTouch : MonoBehaviour
 {
+    private GameObject currentInterObj = null;
+
+    private bool searchingForPlayer = false;
+    Camera m;
+    Aud = m.getComponent()
     // Start is called before the first frame update
     void Start()
     {
-        
+       m.Stop();
+     
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnTriggerEnter2D(Collider2D other)
     {
-        
+        if (other.CompareTag("coin"))
+        {
+
+            currentInterObj = other.gameObject;
+            Destroy(other.gameObject);
+            Score.addToScore();
+        }
+        else
+        {
+            if (other.CompareTag("Enemy"))
+            {
+                Debug.Log("");
+            }
+
+        }
     }
+
+
 }
