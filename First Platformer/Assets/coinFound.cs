c5f55150ce1abb936f5f743a28f7c8dff221d2bd
Jonathan Lin <jlin7500@bths.edu>
2019-06-12 09:54:22 -0400

    not done

--- First Platformer/Assets/coinFound.cs
+++ First Platformer/Assets/coinFound.cs
@@ -8,18 +8,25 @@ public class coinFound : MonoBehaviour
     private GameObject currentInterObj = null;
 
     private bool searchingForPlayer = false;
-
+    public static AudioClip Bonus, Click;
+    static AudioSource audioSrc;
     // Start is called before the first frame update
     void Start()
     {
-
+        Bonus = Resources.Load<AudioClip>("Bonas");
+        Bonus = Resources.Load<AudioClip>("Click");
+        audioSrc = GetComponent<AudioSource>();
     }
-
+    public static void getCoin (string clip)
+    {
+       
+    }
     public void OnTriggerEnter2D(Collider2D other)
     {
+        Debug.Log("sound");
+        audioSrc.PlayOneShot(Bonus);
         if (other.CompareTag("coin"))
         {
-
             currentInterObj = other.gameObject;
             Destroy(other.gameObject);
             Score.addToScore();
@@ -28,6 +35,7 @@ public class coinFound : MonoBehaviour
         {
             if (other.CompareTag("Enemy"))
             {
+                audioSrc.PlayOneShot(Click);
                 Debug.Log("");
             }
            
--- First Platformer/Assets/starTouch.cs
+++ First Platformer/Assets/starTouch.cs
@@ -5,35 +5,7 @@ using UnityEngine.UI;
 
 public class starTouch : MonoBehaviour
 {
-    private GameObject currentInterObj = null;
-
-    private bool searchingForPlayer = false;
-    Camera m;
-    //Aud = m.getComponent();
-    // Start is called before the first frame update
-    void Start()
-    {
-       //m.Stop();
-    }
-
-    public void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.CompareTag("coin"))
-        {
-
-            currentInterObj = other.gameObject;
-            Destroy(other.gameObject);
-            Score.addToScore();
-        }
-        else
-        {
-            if (other.CompareTag("Enemy"))
-            {
-                Debug.Log("");
-            }
-
-        }
-    }
+   
 
 
 }
